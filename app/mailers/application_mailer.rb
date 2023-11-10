60f60d6481633a919aa30f147c7a0e0f331bbd0d
Jacob Michalskie <jacob.michalskie@suse.com>
2023-11-10 11:30:37 +0100

    Set the default mail from value

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: Rails.configuration.site.dig(:mail, :from)
   layout 'mailer'
 end
