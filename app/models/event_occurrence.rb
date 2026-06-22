openSUSE/calendar-o-o

commit 2d50cd201e6b98df31413c7be00861e1289c736b

Validate occurrence date input
Prevent users from adding schedule_occurrences entries without a time,
as NULL time entries cause ICS rendering to fail.

Link: https://progress.opensuse.org/issues/176724
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

app/models/event_occurrence.rb
@@ -6,6 +6,8 @@ class EventOccurrence < ApplicationRecord
 
   belongs_to :event
 
+  validates :time, presence: true
+
   def exclude
     event.schedule_exceptions << ScheduleException.new(time: starts_at)
     event.save!

app/javascript/controllers/occurrence_controller.js
@@ -1,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "destroy", "time" ]
+
+  connect() {
+    this.toggleRequired()
+  }
+
+  toggleRequired() {
+    this.timeTarget.required = !this.destroyTarget.checked
+  }
+}

app/views/schedule_occurrences/_form.html.haml
@@ -1,13 +1,15 @@
 = fields_for "event[schedule_occurrences_attributes][#{index}]", occurrence do |fr|
   = fr.hidden_field(:id, value: fr.object.id) if fr.object
-  .row
+  .row{ data: { controller: 'occurrence' } }
     .col
       .form-floating
-        = fr.datetime_local_field :time, class: 'form-control'
+        = fr.datetime_local_field :time, class: 'form-control', required: true,
+                                         data: { occurrence_target: 'time' }
         = fr.label :time, t('schedule_occurrences.time')
     .col-auto
       .form-destroy
-        = fr.check_box :_destroy, class: 'form-check-input'
+        = fr.check_box :_destroy, class: 'form-check-input',
+                                  data: { occurrence_target: 'destroy', action: 'change->occurrence#toggleRequired' }
         = fr.label :_destroy, t('schedule_occurrences.remove'), class: 'form-check-disabled'
         = fr.label :_destroy, t('schedule_occurrences.restore'), class: 'form-check-enabled'
     %hr.mt-3

commit 23fca01137eb61364e5ac93f944f2f3476c06025

Move time validation to correct model
EventOccurrence does not have :time - ScheduleOccurrence does.

Fixes: 2d50cd201e6b ("Validate occurrence date input")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

app/models/event_occurrence.rb
@@ -6,8 +6,6 @@ class EventOccurrence < ApplicationRecord
 
   belongs_to :event
 
-  validates :time, presence: true
-
   def exclude
     event.schedule_exceptions << ScheduleException.new(time: starts_at)
     event.save!

app/models/schedule_occurrence.rb
@@ -4,6 +4,8 @@
 class ScheduleOccurrence < ApplicationRecord
   belongs_to :event
 
+  validates :time, presence: true
+
   def time
     super&.in_time_zone(event.timezone)
   end